03ef42ce3526d42bdbd020aa0515f9389dee5cd8
K-aigoro <aigorokazeem90@gmail.com>
2024-05-31 16:12:37 +0100

    Change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 // import Button from "./Button";
 
 function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
   return (
     <>
       <header className="header">
@@ -10,13 +15,18 @@ function Header() {
           <ion-icon name="logo-stencil"></ion-icon>
           <span>WhiteSandsEvents</span>
         </div>
-        <nav className="nav">
-          <a href="#home">Home</a>
-          <a href="#services">Services</a>
-          <a href="#about">About</a>
-          <a href="#event">EventGallery</a>
-        </nav>
-        <Button text="Book Event" className="work-button" />
+        <div className="navbar">
+          <nav className={`nav ${isNavOpen ? "open" : ""}`}>
+            <a href="#home">Home</a>
+            <a href="#services">Services</a>
+            <a href="#about">About</a>
+            <a href="#event">EventGallery</a>
+          </nav>
+          <Button text="Book Event" className="work-button1" />
+        </div>
+        <div className="nav-toggle" onClick={toggleNav}>
+          &#9776;
+        </div>
       </header>
     </>
   );
